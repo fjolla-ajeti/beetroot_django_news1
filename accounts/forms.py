62763b81dc3bacf657e39879ac10a3236353afbc
fjolla-ajeti <fjollaajeti9@gmail.com>
2024-01-10 19:12:57 +0100

    first-commit

---
+++ accounts/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    model = CustomUser
+    fields = UserCreationForm.Meta.fields + ('age',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model=CustomUser
+        fields = UserChangeForm.Meta.fields
